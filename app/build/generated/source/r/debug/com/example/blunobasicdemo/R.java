3f8720face036ef558471a4a0b8739466b6bf25d
bn3101group7 <bn3101.group7@gmail.com>
2014-12-09 11:58:51 +0800

    changes to build files after Android studio update

--- app/build/generated/source/r/debug/com/example/blunobasicdemo/R.java
+++ app/build/generated/source/r/debug/com/example/blunobasicdemo/R.java
@@ -40,13 +40,12 @@ public final class R {
         public static final int hair_06=0x7f020016;
         public static final int ic_launcher=0x7f020017;
         public static final int logo=0x7f020018;
-        public static final int logo_2=0x7f020019;
-        public static final int skin_1=0x7f02001a;
-        public static final int skin_2=0x7f02001b;
-        public static final int skin_3=0x7f02001c;
-        public static final int skin_4=0x7f02001d;
-        public static final int skin_5=0x7f02001e;
-        public static final int skin_6=0x7f02001f;
+        public static final int skin_1=0x7f020019;
+        public static final int skin_2=0x7f02001a;
+        public static final int skin_3=0x7f02001b;
+        public static final int skin_4=0x7f02001c;
+        public static final int skin_5=0x7f02001d;
+        public static final int skin_6=0x7f02001e;
     }
     public static final class id {
         public static final int action_settings=0x7f080028;
